cd0135f764b392c1bd9692ea23d24590212d9385
Carlos Henrique Escouto <carloshenriqueescouto@gmail.com>
2020-03-15 23:03:46 -0300

    d

--- lib/flare.dart
+++ lib/flare.dart
@@ -1,19 +1,73 @@
+import 'package:flare_flutter/flare.dart';
 import 'package:flare_flutter/flare_actor.dart';
+import 'package:flare_flutter/flare_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flare_dart/math/mat2d.dart';
+import 'package:test_video/video.dart';
 
 class Flare extends StatefulWidget {
+
+  final bool isLoading;
+  final VoidCallback indicatedCompletion;
+  Flare(this.isLoading, {this.indicatedCompletion});
+
   @override
   _FlareState createState() => _FlareState();
 }
 
-class _FlareState extends State<Flare> {
-  @override
-  Widget build(BuildContext context) {
-    return FlareActor(
-      "assets/navbar.flr",
-      alignment:Alignment.bottomCenter,
-      fit:BoxFit.contain,
-      animation:"0",
+class _FlareState extends State<Flare> 
+  implements FlareController {
+
+    PageController _controller = PageController(initialPage: 1);
+
+    @override
+    Widget build(BuildContext context) {
+
+    final List<Widget> list = [
+      Video(),
+      Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Color.fromRGBO(255, 255, 255, 1),
+      )
+    ];
+
+    final PageView pageView = PageView(
+      children: list,
+      controller: _controller,
+      onPageChanged: (int index) => setState(() => print(index)),
+    );
+
+    return Scaffold(
+      bottomNavigationBar: FlareActor(
+        "assets/navbar.flr",
+        alignment:Alignment.bottomCenter,
+        // fit:BoxFit.contain,
+        animation: '0',
+        // controller: this,
+      ),
+      body: pageView
     );
   }
+
+  ValueNotifier<bool> isActive;
+  // ActorAnimation _animations;
+  // double _animationTime = 0.0;
+
+  @override
+  bool advance(FlutterActorArtboard artboard, double elapsed) {
+    // _animationTime += _animations.duration;
+    // _animations.apply(_animationTime, artboard, 1.0);
+
+    return true;
+  }
+
+  @override
+  void initialize(FlutterActorArtboard artboard) {
+    // _animations = artboard.getAnimation("Animations");
+  }
+
+  @override
+  void setViewTransform(Mat2D viewTransform) {}
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:test_video/flare.dart';
 import 'package:test_video/video.dart';
 import 'package:flutter/material.dart';
@@ -11,11 +14,29 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
 
+  bool _isLoading;
+  @override
+  void initState() {
+    // Start loading...
+    _isLoading = true;
+
+    // This would be based off of some async action, for now just complete
+    // loading after some random time between 1 and 3 seconds.
+    // We use a random time to show that the animation will wait for the loop
+    // to complete properly.
+    Timer(Duration(milliseconds: 1000+Random().nextInt(2000)), () {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // home: Flare(),
-      home: Video(),
+      home: Flare(_isLoading, indicatedCompletion: () => print('Carlos')),
+      // home: Video(),
     );
   }
 
